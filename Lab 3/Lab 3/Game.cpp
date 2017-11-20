7cd0e1047bed0da28096dcdb98ec4ed90466f1be
(Student) - Aoife Powders <c00218374@ITCARLOW.IE>
2017-11-20 12:08:37 +0000

    Lab3

---
+++ Lab 3/Lab 3/Game.cpp
@@ -0,0 +1,211 @@
+// author Aoife Powders
+
+
+#include "Game.h"
+#include <iostream>
+
+
+
+Game::Game() :
+	m_window{ sf::VideoMode{ 800, 800, 32 }, "SFML Game" },
+	m_exitGame{false} //when true game will exit
+{
+	setupFontAndText(); // load font 
+	setUpPlane();
+	setUpCircle();
+	Ux = U * std::cos(angle);
+	Uy = U * std::sin(angle);
+	velocity = { Ux, Uy };
+	gravity *= pixelsToMetres;
+	ogVelocity = velocity;
+	position = ogPosition;
+	velocity = ogVelocity;
+	angleDegrees = angle * (180 / 3.14);
+}
+
+
+Game::~Game()
+{
+}
+
+
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	sf::Time timePerFrame = sf::seconds(1.f / 60.f); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeChange = timeSinceLastUpdate.asSeconds();
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+
+void Game::processEvents()
+{
+	sf::Event event;
+	while (m_window.pollEvent(event))
+	{
+		if ( sf::Event::Closed == event.type) // window message
+		{
+			m_window.close();
+		}
+		if (sf::Event::KeyPressed == event.type) //user key press
+		{
+			if (sf::Keyboard::Escape == event.key.code)
+			{
+				m_exitGame = true;
+			}
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+		{
+			move = true;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::B))
+		{
+			position = ogPosition;
+			velocity = ogVelocity;
+			velocity = { Ux, Uy };
+			velocityText.setString("Velocity:" + std::to_string(U));
+			angleText.setString("Angle:" + std::to_string(angleDegrees)+ "Degrees");
+			move = false;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::V))
+		{
+			U++;
+			Ux = U * cos(angle);
+			Uy = U * sin(angle);
+			velocityText.setString("Velocity:" + std::to_string(U));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+		{
+			U--;
+			Ux = U * cos(angle);
+			Uy = U * sin(angle);
+			velocityText.setString("Velocity:" + std::to_string(U));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::U))
+		{
+			angle-=0.1;
+			angleDegrees = angle * (180 / 3.14);
+			Ux = U * cos(angle);
+			Uy = U * sin(angle);
+			angleText.setString("Angle:" + std::to_string(angleDegrees) + "Degrees");
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y))
+		{
+			angle+=0.1;
+			angleDegrees = angle * (180 / 3.14);
+			Ux = U * cos(angle);
+			Uy = U * sin(angle);
+			angleText.setString("Angle:" + std::to_string(angleDegrees) + "Degrees");
+		}
+	}
+}
+
+void Game::update(sf::Time t_deltaTime)
+{
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+	gravityfunc();
+	shape.setPosition(position);	
+	
+}
+
+void Game::render()
+{
+	m_window.clear(sf::Color::Black);
+	m_window.draw(m_plane);
+	m_window.draw(shape);
+	m_window.draw(target);
+	m_window.draw(velocityText);
+	m_window.draw(angleText);
+	m_window.display();
+}
+
+void Game::setupFontAndText()
+{
+	if (!m_font.loadFromFile("ASSETS//FONTS//Halloween.ttf"))
+	{
+		std::cout << "Font not loaded" << std::endl;
+	}
+	
+	velocityText.setFont(m_font); 
+	velocityText.setString("Velocity:" + std::to_string(U));
+	velocityText.setCharacterSize(20);
+	velocityText.setPosition(100, 100);
+	velocityText.setFillColor(sf::Color::White);  
+
+	angleText.setFont(m_font);
+	angleText.setString("Angle:" + std::to_string(angle) + " Degrees");
+	angleText.setCharacterSize(20);
+	angleText.setPosition(100, 150);
+	angleText.setFillColor(sf::Color::White);
+
+}
+
+void Game::airResistance()
+{
+	ma = mass * accel;
+	ma = (mass * grav) - airRes;
+}
+
+void Game::gravityfunc()
+{
+	if (move == true)
+	{
+		
+		position.x = position.x + velocity.x * timeChange + (0.5 * gravity.x * (timeChange * timeChange));
+		position.y = position.y + velocity.y * timeChange + (0.5 * gravity.y * (timeChange * timeChange));
+
+		velocity.y = velocity.y + gravity.y * timeChange;
+		velocity.x = velocity.x + gravity.x * timeChange;
+
+		// out put the time it takes to get off the ground and back on it
+		timer++;
+		if (timer == 60)
+		{
+			timer = 0;
+			actualTimer++;
+		}
+
+	}
+	//stops the pixel when it hits the plane
+	if (position.y > planePos.y)
+	{
+		move = false;
+
+		velocity.y = -.9 * velocity.y;
+	}
+}
+
+void Game::setUpPlane()
+{
+	m_plane.setFillColor(sf::Color::Green);
+	m_plane.setPosition(planePos);
+	m_plane.setSize(size);
+}
+
+void Game::setUpCircle()
+{
+	shape.setFillColor(sf::Color::White);
+	shape.setPosition(position);
+	shape.setRadius(5);
+
+	target.setFillColor(sf::Color::Magenta);
+	target.setPosition(600, 400);
+	target.setRadius(10);
+
+}
+
+
---
+++ Lab 3/Lab 3/Game.h
@@ -0,0 +1,70 @@
+// author Peter Lowe
+#ifndef GAME
+#define GAME
+
+#include <SFML/Graphics.hpp>
+#include <cmath>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// main method for game
+	void run();
+
+private:
+
+	void processEvents();
+	void update(sf::Time t_deltaTime);
+	void render();
+
+	void gravityfunc();
+	void setUpPlane();
+	void setUpCircle();
+	void setupFontAndText();
+	void airResistance();
+
+	sf::CircleShape shape{ 0.5f };
+	sf::CircleShape target{ 1.0f };
+	sf::RectangleShape m_plane;
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_font; // font used by message
+	sf::Text velocityText;
+	sf::Text angleText;
+
+	bool m_exitGame; // control exiting game
+	bool move = false;
+
+	float angle = -0.785398;
+	float angleDegrees;
+	float Ux{ 0 };
+	float Uy{ 0 };
+	float U = 100;
+	float Furthest = 0;
+
+	sf::Vector2f size{ 800, 100 };
+	sf::Vector2f ogVelocity{ 90, -190.27 };
+	sf::Vector2f velocity;
+	sf::Vector2f ogPosition{ 100, 698 };
+	sf::Vector2f position{ 100, 698 };
+	sf::Vector2f planePos{ 1, 700 };
+	sf::Vector2f gravity{ 0.0f, 9.8f };
+
+	float mass = 50;
+	float airRes = 10;
+	float accel = 0;
+	float grav = 9.8;
+	float ma = 0;
+
+	float heighest = 0;
+	int timer = 0;
+	int actualTimer = 0;
+	float timeChange;
+	float pixelsToMetres = 10;
+
+};
+
+#endif // !GAME
+
